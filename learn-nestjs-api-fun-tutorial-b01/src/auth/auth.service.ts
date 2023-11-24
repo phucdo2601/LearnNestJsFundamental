a0940304009ead675bdfbe5fafb429b39e0a669d
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-11-24 09:53:43 +0700

    [learn-basic-fun-nestjs-pro-b01] 05. Basic complete signup login function b01

--- learn-nestjs-api-fun-tutorial-b01/src/auth/auth.service.ts
+++ learn-nestjs-api-fun-tutorial-b01/src/auth/auth.service.ts
@@ -1,46 +1,57 @@
 import { AuthDto } from './dto/auth.dto';
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({
 
 })
 export class AuthService {
     constructor(private prisma: PrismaService) {
-        
+
     }
 
     async signup(dto: AuthDto) {
         //generate the password hash
         const hash = await argon.hash(dto.password);
-        
 
-        //save the new user in db
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash: hash,
-            },
+        try {
+            //save the new user in db
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash: hash,
+                },
+
+                /**
+                 * Select specific fields on table
+                 */
+                // select: {
+                //     id: true,
+                //     email: true,
+                //     createdAt: true,
+                //     updatedAt: true
+                // }
+            })
 
             /**
-             * Select specific fields on table
-             */
-            // select: {
-            //     id: true,
-            //     email: true,
-            //     createdAt: true,
-            //     updatedAt: true
-            // }
-        })
+                 * Delete the specific fields of object
+                 */
+            delete user.hash
 
-        /**
-             * Delete the specific fields of object
-             */
-        delete user.hash
 
-        //return the saved user
-        return user;
+            //return the saved user
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === "P2002") {
+                    throw new ForbiddenException('Credentials token');
+                }
+            }
+            throw error;
+        }
+
     }
 
     signin() {
