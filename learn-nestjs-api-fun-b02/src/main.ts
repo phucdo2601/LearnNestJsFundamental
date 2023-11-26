b8d4f3f1cfa3c60455b2aea137747132adda19da
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-11-26 21:52:45 +0700

    [learn-basic-fun-nestjs-pro-api-b02] 04. Basic setup implement Swagger-UI in nestjs project b01

--- learn-nestjs-api-fun-b02/src/main.ts
+++ learn-nestjs-api-fun-b02/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Learn-Fun-Nestjs-Api-Swagger')
+    .setDescription('Swagger Description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3030);
 }
 bootstrap();
