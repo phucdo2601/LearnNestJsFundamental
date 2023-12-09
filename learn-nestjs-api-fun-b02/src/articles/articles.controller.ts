a2abb9110651da43603661823b72998800ded918
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-12-09 15:11:46 +0700

    [learn-basic-fun-nestjs-pro-api-b02] 09 Basic setup title and description model in swagger b01

--- learn-nestjs-api-fun-b02/src/articles/articles.controller.ts
+++ learn-nestjs-api-fun-b02/src/articles/articles.controller.ts
@@ -2,40 +2,59 @@ import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/commo
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ArticleEntity } from './entities/article.entity';
 
 @Controller('articles')
+@ApiTags('articles')
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
 
   @Get()
+  @ApiOkResponse({
+    type: ArticleEntity, isArray: true,
+  })
   findAll() {
     return this.articlesService.findAll();
   }
 
   @Get('drafts')
+  @ApiOkResponse({
+    type: ArticleEntity, isArray: true,
+  })
   async findArticleDraft() {
     return await this.articlesService.findArticleDrafts();
   }
 
   //get article by id
   @Get(':id')
+  @ApiOkResponse({
+    type: ArticleEntity, 
+  })
   async findArticleById(@Param('id') articleId: string) {
     return await this.articlesService.findOne(+articleId);
   }
 
   //create new article
   @Post()
+  @ApiCreatedResponse({
+    type: ArticleEntity,
+  })
   async createNewArticle(@Body() createActicle: CreateArticleDto) {
     return await this.articlesService.createNewArticle(createActicle);
   }
 
   @Patch(':id')
+  @ApiOkResponse({
+    type: ArticleEntity,
+  })
   async updateArticle(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
     return await this.articlesService.updateArticle(+id, updateArticleDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: ArticleEntity })
   async deleteArticle(@Param('id') id: string) {
     return await this.articlesService.deleteArticle(+id);
   }
--- learn-nestjs-api-fun-b02/src/articles/entities/article.entity.ts
+++ learn-nestjs-api-fun-b02/src/articles/entities/article.entity.ts
@@ -1,1 +1,29 @@
-export class Article {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Article } from "@prisma/client";
+
+export class ArticleEntity implements Article {
+    
+    @ApiProperty()
+    id: number;
+
+    @ApiProperty()
+    title: string;
+
+    @ApiProperty({
+        required: false,
+        nullable: true,
+    })
+    description: string | null;
+
+    @ApiProperty()
+    body: string;
+
+    @ApiProperty()
+    published: boolean;
+
+    @ApiProperty()
+    createAt: Date;
+
+    @ApiProperty()
+    updatedAt: Date;
+}
