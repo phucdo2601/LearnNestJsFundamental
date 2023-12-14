998255ffb5aab19c29bddab1cf005ac43ae91436
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-12-14 15:56:14 +0700

    [learn-basic-fun-nestjs-api-ec-web-b01] 13. Basic create productDto b01

--- listNestPro/les01-ec-web-b01/l-nest-api-e-commerce-web-b01/src/products/dto/create-product.dto.ts
+++ listNestPro/les01-ec-web-b01/l-nest-api-e-commerce-web-b01/src/products/dto/create-product.dto.ts
@@ -1,1 +1,71 @@
-export class CreateProductDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsArray, IsNotEmpty, IsNumber, IsPositive, IsString, Min } from "class-validator";
+
+export class CreateProductDto {
+    
+    @ApiProperty()
+    @IsNotEmpty({
+        message: "title can not be blank."
+    })
+    @IsString({
+
+    })
+    title: string;
+
+    @ApiProperty()
+    @IsNotEmpty({
+        message: "description can not be blank"
+    })
+    @IsString({
+
+    })
+    description: string;
+
+    @ApiProperty()
+    @IsNotEmpty({
+        message: "price should not be empty."
+    })
+    @IsNumber({
+        maxDecimalPlaces: 2
+    }, {
+        message: 'price should be number & max decimal precission 2.'
+    })
+    @IsPositive({
+        message: "price should be positive number."
+    })
+    price: number;
+
+    @ApiProperty()
+    @IsNotEmpty({
+        message: "stock should not be empty."
+    })
+    @IsNumber({
+        
+    }, {
+        message: 'stock should be number.'
+    })
+    @Min(0, {
+        message: "Price cannot be negative."
+    })
+    stock: number;
+
+    @ApiProperty()
+    @IsNotEmpty({
+        message: "images should not be empty."
+    })
+    @IsArray({
+        message: "images should be in array format."
+    })
+    images: string[];
+
+    @ApiProperty()
+    @IsNotEmpty({
+        message: "category should not be empty."
+    })
+    @IsNumber({
+        
+    }, {
+        message: 'category should be number.'
+    })
+    category: number;
+}
